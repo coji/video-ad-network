16aa987d81a9c993f6d3ecb3a9db332a1e20f5d8
coji <coji@techtalk.jp>
2024-12-10 15:53:16 +0900

    feat: update package.json dependencies to use catalog references and refactor route configuration in Vite setup

--- apps/ui/app/routes.ts
+++ apps/ui/app/routes.ts
@@ -1,9 +1,6 @@
-import type { RouteConfig } from '@remix-run/route-config'
 import { remixRoutesOptionAdapter } from '@remix-run/routes-option-adapter'
 import { flatRoutes } from 'remix-flat-routes'
 
-export const routes: RouteConfig = remixRoutesOptionAdapter((defineRoutes) =>
-	flatRoutes('routes', defineRoutes, {
-		ignoredRouteFiles: ['**/index.ts'],
-	}),
+export default remixRoutesOptionAdapter((defineRotue) =>
+	flatRoutes('routes', defineRotue),
 )
