faed7baf70bfbd03fc44efe05377ae9ec423cafa
coji <coji@techtalk.jp>
2024-12-22 10:39:13 +0900

    fix: update PIXEL type to ArrayBuffer in pixel.ts

--- apps/ad-server/src/functions/pixel.ts
+++ apps/ad-server/src/functions/pixel.ts
@@ -1,5 +1,5 @@
 // 1x1透明GIF画像のバイナリデータ
-export const PIXEL = Buffer.from(
+export const PIXEL: ArrayBuffer = Buffer.from(
   'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
   'base64',
-)
+).buffer
